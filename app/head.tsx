c5db50519c51c8be062b68faad40b561a2891f0b
Muaz Erdem Yigit <muazerdemyigit@hotmail.com>
2023-03-25 15:29:00 +0300

    little changes

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,16 @@
 export default function Head() {
     return (
         <>
-            <title>Muaz Erdem Yiğit's Blog</title>
+            <title>
+                Muaz Erdem Yiğit's Blog
+            </title>
 
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
             <link rel="icon" href="/icon.svg" />
         </>
+
     );
 }
